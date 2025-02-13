4c61ffd42446796bb205a57be712c54d13bda509
Kateryna <kharchenko.k@slm.ua>
2025-02-13 21:32:12 +0200

    Додавання папок lesson-3 та lesson-4

---
+++ lesson-4/task-3.js
@@ -0,0 +1,5 @@
+let age = 35; 
+let isAdult = age >= 18; 
+
+console.log("Age:", age); 
+console.log("The person is an adult?", isAdult);
---
+++ lesson-4/task2.js
@@ -0,0 +1,9 @@
+let name1 = "Nikki";
+let name2 = "Anna";
+
+let greeting = "Привіт, " + name1 + " і " + name2 + "!";
+console.log(greeting); 
+
+
+let greetingTemplate = `Hello, ${name1} and ${name2}!`;
+console.log(greetingTemplate);
